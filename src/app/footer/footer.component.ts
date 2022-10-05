2ecfba1a20dc69308ab276afb00d83ad4d9e8d62
cheat2001 <chansocheatsok2001@gmail.com>
2022-10-05 16:46:37 +0700

    fix more conflict

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,12 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./footer.component.css']
-=======
   styles: [
   ]
->>>>>>> 959683a9ec5d43ea870d3b8a97df3b3c363e806f
 })
 export class FooterComponent implements OnInit {
 
