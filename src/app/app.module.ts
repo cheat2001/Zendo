76e4d556257356822140d419a4d56532c7055158
cheat2001 <chansocheatsok2001@gmail.com>
2022-10-10 13:57:37 +0700

    adding routing to master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,22 @@ import { LoginComponent } from './login/login.component';
 
 import { FooterComponent } from './footer/footer.component';
 import { SlidenavComponent } from './slidenav/slidenav.component';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
 
-    LoginComponent
+    LoginComponent,
     FooterComponent,
     SlidenavComponent
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{path:'home',component:SlidenavComponent},
+  {path:'login',component:LoginComponent}
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
